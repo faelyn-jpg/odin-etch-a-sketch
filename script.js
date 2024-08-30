883abc9d0ec149ec614c34d9376dce145afeed5f
faelyn-jpg <169906179+faelyn-jpg@users.noreply.github.com>
2024-08-30 12:29:55 +1200

    Add drag functionality to drawing

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const boxContainer = document.querySelector('.box-container')
+const gridBox = document.querySelector('.gridBox')
+let isToggling = false
 
 function addGlobalEventListener(type, selector, callback, parent = document) {
   parent.addEventListener(type, (e) => {
@@ -7,21 +9,65 @@ function addGlobalEventListener(type, selector, callback, parent = document) {
     }
   })
 }
-
 function addGrid() {
   for (let i = 0; i < 256; i++) {
     const box = document.createElement('div')
+    box.setAttribute('ondragstart', 'handleDrag(event)')
     box.classList.add('gridBox', 'onHover')
     boxContainer.appendChild(box)
   }
 }
 
+function handleDrag(event) {
+  event.preventDefault()
+  console.log('Event default prevented')
+}
+function enableToggle() {
+  console.log('enableToggle')
+  return true
+}
+
+function disableToggle() {
+  console.log('disableToggle')
+  return false
+}
+
 addGrid()
-// addGlobalEventListener(
-//   'mouseover',
-//   '.gridBox',
-//   (e) => {
-//     e.target.className += ' onHover'
-//   },
-//   boxContainer
-// )
+
+addGlobalEventListener(
+  'mousedown',
+  '.gridBox',
+  (e) => {
+    isToggling = enableToggle()
+    console.log(isToggling)
+  },
+  boxContainer
+)
+addGlobalEventListener(
+  'mouseup',
+  '.gridBox',
+  (e) => {
+    isToggling = disableToggle()
+    console.log(isToggling)
+  },
+  boxContainer
+)
+addGlobalEventListener(
+  'mouseover',
+  '.gridBox',
+  (e) => {
+    if (isToggling === true) {
+      e.target.classList.add('drawnBox')
+    } else return
+  },
+  boxContainer
+)
+
+addGlobalEventListener(
+  'mousedown',
+  '.gridBox',
+  (e) => {
+    e.target.classList.add('drawnBox')
+  },
+  boxContainer
+)
