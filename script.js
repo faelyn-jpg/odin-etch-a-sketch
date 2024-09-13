0b11804956cc3f2f5612405ec7b5d20fb342d94e
faelyn-jpg <169906179+faelyn-jpg@users.noreply.github.com>
2024-09-13 14:24:27 +1200

    add new grid creation feature fix bug where cant draw on grid after new grid created fix bug where you keep drawing if you go outside the container fix bug where you can add too many boxes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const boxContainer = document.querySelector('.box-container')
 const gridBox = document.querySelector('.gridBox')
-const body = document.querySelector('body')
+const body = document.querySelector('.container-wrapper')
 let isToggling = false
 const containerSize = 735
 let amountOfBoxes = 256
@@ -15,7 +15,7 @@ function addGlobalEventListener(type, selector, callback, parent = document) {
 }
 
 function calculateBoxSize(containerSize, gridSize) {
-  const boxCalc = containerSize + 1 - (gridSize - 1)
+  const boxCalc = containerSize + 1 - gridSize
   const boxWidthAndHeight = boxCalc / gridSize
   return boxWidthAndHeight
 }
@@ -25,27 +25,72 @@ function createCustomGrid() {
     'What size grid would you like? Grid will be created number x number, enter a number from 1 to 100.',
     '32'
   )
-  boxWidthAndHeight = calculateBoxSize(containerSize, gridSize)
-  // removeGrid()
+  if (gridSize > 100 || gridSize < 0) {
+    do {
+      gridSize = prompt('Please enter a valid number from 1 to 100')
+    } while (gridSize > 100 || gridSize < 0)
+  }
+  removeGrid()
   amountOfBoxes = gridSize * gridSize
-
-  //dynamically change css rules in addGrid()
-  addGrid(amountOfBoxes)
+  boxWidthAndHeight = calculateBoxSize(containerSize, gridSize)
+  addGrid(amountOfBoxes, boxWidthAndHeight)
 }
 //propmt user for grid size
 //calculate grid size
 //run add grid with calculated grid w/h
 
-function addGrid(amountOfBoxes) {
+function addGrid(amountOfBoxes, boxWidthAndHeight) {
   for (let i = 0; i < amountOfBoxes; i++) {
+    const boxContainer = document.querySelector('.box-container')
     const box = document.createElement('div')
     box.setAttribute('ondragstart', 'handleDrag(event)')
+    box.style.cssText =
+      'width: ' + boxWidthAndHeight + 'px; height: ' + boxWidthAndHeight + 'px;'
     box.classList.add('gridBox', 'onHover')
+    addGlobalEventListener(
+      'mousedown',
+      '.gridBox',
+      (e) => {
+        isToggling = true
+      },
+      document
+    )
+    addGlobalEventListener(
+      'mouseup',
+      '.gridBox',
+      (e) => {
+        isToggling = false
+      },
+      document
+    )
+    addGlobalEventListener(
+      'mouseover',
+      '.gridBox',
+      (e) => {
+        if (isToggling === true) {
+          e.target.classList.add('drawnBox')
+        } else return
+      },
+      boxContainer
+    )
+    addGlobalEventListener(
+      'mousedown',
+      '.gridBox',
+      (e) => {
+        e.target.classList.add('drawnBox')
+      },
+      boxContainer
+    )
+    boxContainer.addEventListener('mouseleave', (e) => {
+      isToggling = false
+    })
+
     boxContainer.appendChild(box)
   }
 }
 
 function removeGrid() {
+  const boxContainer = document.querySelector('.box-container')
   boxContainer.remove()
   const addContainer = document.createElement('div')
   addContainer.classList.add('box-container')
@@ -54,64 +99,16 @@ function removeGrid() {
 
 function handleDrag(event) {
   event.preventDefault()
-  console.log('Event default prevented')
-}
-function enableToggle() {
-  console.log('enableToggle')
-  return true
-}
-
-function disableToggle() {
-  console.log('disableToggle')
-  return false
 }
 
 let button = document.querySelector('click-button')
-addGrid(amountOfBoxes)
+addGrid(amountOfBoxes, boxWidthAndHeight)
 
 addGlobalEventListener(
   'click',
   'button',
   (e) => {
-    removeGrid()
+    createCustomGrid()
   },
   document
-)
-
-addGlobalEventListener(
-  'mousedown',
-  '.gridBox',
-  (e) => {
-    isToggling = enableToggle()
-    console.log(isToggling)
-  },
-  boxContainer
-)
-addGlobalEventListener(
-  'mouseup',
-  '.gridBox',
-  (e) => {
-    isToggling = disableToggle()
-    console.log(isToggling)
-  },
-  boxContainer
-)
-addGlobalEventListener(
-  'mouseover',
-  '.gridBox',
-  (e) => {
-    if (isToggling === true) {
-      e.target.classList.add('drawnBox')
-    } else return
-  },
-  boxContainer
-)
-
-addGlobalEventListener(
-  'mousedown',
-  '.gridBox',
-  (e) => {
-    e.target.classList.add('drawnBox')
-  },
-  boxContainer
 )
